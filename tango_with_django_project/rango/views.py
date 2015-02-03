97c202e92931551fc04c9c5e882179bfc48ad658
Kristian Sonev <krisrsd@gmail.com>
2015-02-03 20:27:21 +0000

    Chapter 11 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -7,6 +7,7 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
+from datetime import datetime
 
 
 def index(request):
@@ -21,11 +22,74 @@ def index(request):
     context_dict = {'categories': category_list,
                     'pages': pages_list
                     }
-    return render(request, 'rango/index.html', context_dict)
+
+    # COOKIE HANDLING
+    # SERVER SIDE COOKIES
+    visits = request.session.get('visits')
+    if not visits:
+        visits = 1
+
+    reset_last_visit_time = False
+
+    last_visit = request.session.get('last_visit')
+    if last_visit:
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+
+        if (datetime.now() - last_visit_time).seconds > 0:
+            # Append 1 to the cookie value
+            visits += 1
+            reset_last_visit_time = True
+
+    else:
+        # Cookie last_visit does not exist so create it
+        reset_last_visit_time = True
+
+    if reset_last_visit_time:
+        request.session['last_visit'] = str(datetime.now())
+        request.session['visits'] = visits
+
+    context_dict['visits'] = visits
+    response = render(request, 'rango/index.html', context_dict)
+    return response
+
+    # ------------- CLIENT SIDE COOKIES --------------------
+    # Get visits cookie; if it exists -> cast to int, otherwise default at 1
+    # ALL COOKIE VALUES ARE RETURNED AS STRINGS; CAST THEM!
+    # visits = int(request.COOKIES.get('visits', '1'))
+    #
+    # reset_last_visit_time = False
+    # context_dict['visits'] = visits
+    # response = render(request, 'rango/index.html', context_dict)
+    # if 'last_visit' in request.COOKIES:
+    #     last_visit = request.COOKIES['last_visit']
+    #     # Cast the last visit to a python datetime object
+    #     last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+    #
+    #     # If it has been more than a day since the last visit, increment visits
+    #     if (datetime.now() - last_visit_time).days < 0:
+    #         visits += 1
+    #         # .. flag that the last_visits cookie needs to be updated
+    #         reset_last_visit_time = True
+    # else:
+    #     # Cookie last_visit does not exist so flag that it should be set
+    #     reset_last_visit_time = True
+    #     context_dict['visits'] = visits
+    #
+    #     response = render(request, 'rango/index.html', context_dict)
+    #
+    # if reset_last_visit_time:
+    #     # Here it does not matter what type we pass in; it will be automatically cast to a string
+    #     response.set_cookie('last_visit', datetime.now())
+    #     response.set_cookie('visits', visits)
 
 
 def about(request):
-    return render(request, 'rango/about.html', {})
+    if request.session.get('visits'):
+        count = request.session.get('visits')
+    else:
+        count = 9
+
+    return render(request, 'rango/about.html', {'visits': count})
 
 
 def category(request, category_name_slug):
